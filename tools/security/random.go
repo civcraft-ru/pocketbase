7c6392ab5d53c9161e812ecf8770fadef6f767dd
julipup <dingolediego@gmail.com>
2023-08-26 14:02:51 +0300

    Generate UUID for model id instead of random string

--- tools/security/random.go
+++ tools/security/random.go
@@ -5,6 +5,8 @@ import (
 	"math/big"
 	mathRand "math/rand"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 const defaultRandomAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
@@ -37,6 +39,11 @@ func RandomStringWithAlphabet(length int, alphabet string) string {
 	}
 
 	return string(b)
+}
+
+// RandomUUIDString generates a cryptographically random UUID string.
+func RandomUUIDString() string {
+	return uuid.New().String()
 }
 
 // PseudorandomString generates a pseudorandom string with the specified length.
